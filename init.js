fe17e5be6906b644f0b24ea999843d9e2eb77862
The12Forest <manuel@steka.ch>
2026-01-23 09:33:09 +0100

    asdf

--- init.js
+++ init.js
@@ -113,7 +113,7 @@ console.log = function (message, ...optionalParams) {
 
 // console.clear();
 console.log("Server Startup!");
-fetch(baseurl + "/api/main/load");
+// fetch(baseurl + "/api/main/load");
 // fetch(baseurl + "/api/ai/load");
 
 
